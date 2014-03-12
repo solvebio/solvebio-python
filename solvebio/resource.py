7b9cbeb4f565ac1e0a0af98d3c1fa948694441f5
David Caplan <dcaplan@gmail.com>
2014-03-12 12:47:33 -0400

    better handling of full_name as ID; add UpdateableAPIResource to support resource updates; allow querying on datasets that have not been retrieved

--- solvebio/resource.py
+++ solvebio/resource.py
@@ -48,14 +48,21 @@ def convert_to_solve_object(resp):
 
 class SolveObject(dict):
     """Base class for all SolveBio API resource objects"""
+    ALLOW_FULL_NAME_ID = False  # Treat full_name parameter as an ID?
 
     def __init__(self, id=None, **params):
         super(SolveObject, self).__init__()
 
+        # store manually updated values for partial updates
+        self._unsaved_values = set()
+
         if id:
             self['id'] = id
-        elif params.get('full_name'):
+        elif self.ALLOW_FULL_NAME_ID and params.get('full_name'):
             self['full_name'] = params.get('full_name')
+            # no ID was provided so temporarily set the id as full_name
+            # this will get updated when the resource is refreshed
+            self['id'] = params.get('full_name')
 
     def __setattr__(self, k, v):
         if k[0] == '_' or k in self.__dict__:
@@ -72,6 +79,10 @@ class SolveObject(dict):
         except KeyError, err:
             raise AttributeError(*err.args)
 
+    def __setitem__(self, k, v):
+        super(SolveObject, self).__setitem__(k, v)
+        self._unsaved_values.add(k)
+
     @classmethod
     def construct_from(cls, values):
         """Used to create a new object from an HTTP response"""
@@ -81,6 +92,8 @@ class SolveObject(dict):
 
     def refresh_from(self, values):
         self.clear()
+        self._unsaved_values = set()
+
         for k, v in values.iteritems():
             super(SolveObject, self).__setitem__(
                 k, convert_to_solve_object(v))
@@ -98,7 +111,8 @@ class SolveObject(dict):
         if isinstance(self.get('id'), int):
             ident_parts.append('id=%d' % (self.get('id'),))
 
-        if isinstance(self.get('full_name'), unicode):
+        if self.ALLOW_FULL_NAME_ID and \
+                isinstance(self.get('full_name'), unicode):
             ident_parts.append('full_name=%s' % (self.get('full_name'),))
 
         return '<%s at %s> JSON: %s' % (
@@ -109,7 +123,7 @@ class SolveObject(dict):
 
     @property
     def solvebio_id(self):
-        return self.id or self.full_name
+        return self.id
 
 
 class APIResource(SolveObject):
@@ -144,13 +158,10 @@ class APIResource(SolveObject):
     def instance_url(self):
         """Get instance URL by ID or full name (if available)"""
         id = self.get('id')
-        full_name = self.get('full_name')
         base = self.class_url()
 
         if id:
-            return "%s/%d" % (base, id)
-        elif full_name:
-            return "%s/%s" % (base, full_name)
+            return '/'.join([base, unicode(id)])
         else:
             raise Exception(
                 'Could not determine which URL to request: %s instance '
@@ -243,12 +254,39 @@ class CreateableAPIResource(APIResource):
         return convert_to_solve_object(response)
 
 
+class UpdateableAPIResource(APIResource):
+
+    def save(self):
+        self.refresh_from(self.request('patch', self.instance_url(),
+                                       self.serialize(self)))
+        return self
+
+    def serialize(self, obj):
+        params = {}
+        if obj._unsaved_values:
+            for k in obj._unsaved_values:
+                if k == 'id':
+                    continue
+                params[k] = getattr(obj, k) or ""
+        return params
+
+
+class DeletableAPIResource(APIResource):
+
+    def delete(self, **params):
+        self.refresh_from(self.request('delete', self.instance_url(), params))
+        return self
+
+
+# API resources
+
 class User(SingletonAPIResource):
     pass
 
 
 class Depository(CreateableAPIResource, ListableAPIResource,
-                 SearchableAPIResource):
+                 SearchableAPIResource, UpdateableAPIResource):
+    ALLOW_FULL_NAME_ID = True
     FULL_NAME_REGEX = r'^[\w\d\-\.]+$'
 
     @classmethod
@@ -260,7 +298,7 @@ class Depository(CreateableAPIResource, ListableAPIResource,
             if re.match(cls.FULL_NAME_REGEX, _id):
                 params.update({'full_name': _id})
             else:
-                raise Exception('Unrecognized full name.')
+                raise Exception('Unrecognized full name: "%s"' % _id)
 
         return super(Depository, cls).retrieve(id, **params)
 
@@ -277,7 +315,9 @@ class Depository(CreateableAPIResource, ListableAPIResource,
         open_help(self['full_name'])
 
 
-class DepositoryVersion(CreateableAPIResource, ListableAPIResource):
+class DepositoryVersion(CreateableAPIResource, ListableAPIResource,
+                        UpdateableAPIResource):
+    ALLOW_FULL_NAME_ID = True
     FULL_NAME_REGEX = r'^[\w\d\-\.]+/[\w\d\-\.]+$'
 
     @classmethod
@@ -305,8 +345,22 @@ class DepositoryVersion(CreateableAPIResource, ListableAPIResource):
     def help(self):
         open_help(self['full_name'])
 
+    def release(self, released_at=None):
+        """Set the released flag and optional release date and save"""
+        if released_at:
+            self.released_at = released_at
+        self.released = True
+        self.save()
 
-class Dataset(CreateableAPIResource, ListableAPIResource):
+    def unrelease(self):
+        """Unset the released flag and save"""
+        self.released = False
+        self.save()
+
+
+class Dataset(CreateableAPIResource, ListableAPIResource,
+              UpdateableAPIResource):
+    ALLOW_FULL_NAME_ID = True
     FULL_NAME_REGEX = r'^([\w\d\-\.]+/){2}[\w\d\-\.]+$'
 
     @classmethod
@@ -335,7 +389,7 @@ class Dataset(CreateableAPIResource, ListableAPIResource):
                 'up fields')
 
         if name:
-            # construct the field URN
+            # construct the field's full_name if a field name is provided
             return DatasetField.retrieve(
                 '/'.join([self['full_name'], name]))
 
@@ -344,8 +398,14 @@ class Dataset(CreateableAPIResource, ListableAPIResource):
 
     def query(self, **params):
         if 'data_url' not in self:
-            raise Exception(
-                'Please use Dataset.retrieve({ID}) before querying')
+            if 'id' not in self or not self['id']:
+                raise Exception(
+                    'No Dataset ID was provided. '
+                    'Please instantiate the Dataset '
+                    'object with an ID or full_name.')
+            # automatically construct the data_url from the ID
+            self['data_url'] = self.instance_url() + u'/data'
+
         q = Query(self['data_url'], **params)
         if params.get('filters'):
             return q.filter(params.get('filters'))
@@ -355,12 +415,14 @@ class Dataset(CreateableAPIResource, ListableAPIResource):
         open_help(self['full_name'])
 
 
-class DatasetField(CreateableAPIResource, ListableAPIResource):
+class DatasetField(CreateableAPIResource, ListableAPIResource,
+                   UpdateableAPIResource):
+    ALLOW_FULL_NAME_ID = True
     FULL_NAME_REGEX = r'^([\w\d\-\.]+/){3}[\w\d\-\.]+$'
 
     @classmethod
     def retrieve(cls, id, **params):
-        """Supports lookup by URN or full name"""
+        """Supports lookup by ID or full name"""
         if isinstance(id, unicode) or isinstance(id, str):
             _id = unicode(id).strip()
             id = None
