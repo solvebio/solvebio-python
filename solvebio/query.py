28fc7f469daf7308e33873649c4288db9fa14845
rocky <rb@dustyfeet.com>
2014-09-06 09:15:24 -0400

    Document __range and __between in filters

--- solvebio/query.py
+++ solvebio/query.py
@@ -26,8 +26,10 @@ class Filter(object):
 
     Each set of kwargs in a `Filter` are ANDed together:
 
-        * `<field>=''` uses a term filter (exact term)
-        * `<field>__in=[]` uses a terms filter (match any)
+        * `<field>='<value>'` matches if the field that exact value
+        * `<field>__in=[<item1>, ...]` matches any of the terms <item1> and so on
+        * `<field>__range=[<start>, <end>]` matches anything from <start> to <end>
+        * `<field>__range=[<start>, <end>]` matches anything between <start> to <end> not include either <start> or <end>
 
     String terms are not analyzed and are always assumed to be exact matches.
 
