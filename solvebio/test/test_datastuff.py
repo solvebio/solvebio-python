f3e80809bdba3d2b7ba00dc5727bf419629679aa
rocky <rocky@solvebio.com>
2014-10-22 13:47:02 -0400

    Add depostuff tests

--- solvebio/test/test_datastuff.py
+++ solvebio/test/test_datastuff.py
@@ -1,7 +1,6 @@
 """Test Dataset, DatasetField, and Facets"""
 import unittest
 from solvebio.resource import Dataset, DatasetField
-from numbers import Number
 
 
 class DataStuffTest(unittest.TestCase):
@@ -20,17 +19,15 @@ class DataStuffTest(unittest.TestCase):
                          "Retrieving dataset id {0} found by all()"
                          .format(dataset.id))
 
-        for field in ['class_name', 'created_at',
-                      'data_url',
-                      'depository', 'depository_id',
-                      'depository_version', 'depository_version_id',
-                      'description',
-                      'fields_url', 'full_name',
-                      'name', 'title', 'updated_at',
-                      'url']:
-            self.assertTrue(field in dataset,
-                            "Should find field {0} in dataset {1}"
-                            .format(field, dataset.id))
+        check_fields = set(['class_name', 'created_at',
+                            'data_url',
+                            'depository', 'depository_id',
+                            'depository_version', 'depository_version_id',
+                            'description',
+                            'fields_url', 'full_name',
+                            'id', 'name', 'title', 'updated_at',
+                            'url'])
+        self.assertSetEqual(set(dataset.keys()), check_fields)
         fields = dataset.fields()
         if fields.total == 0:
             return unittest.skip('no fields of dataset {0} found'
@@ -46,20 +43,19 @@ class DataStuffTest(unittest.TestCase):
                          "Retrieving DatasetField id {0} found by all()"
                          .format(dataset_field.id))
 
-        for field in ['class_name', 'created_at',
-                      'dataset', 'dataset_id',
-                      'description', 'facets_url',
-                      'name', 'updated_at',
-                      'url']:
-            self.assertTrue(field in dataset_field,
-                            'Should find field {0} in fields {1}'
-                            .format(field, dataset_field.id))
+        check_fields = set(['class_name', 'created_at',
+                            'data_type', 'dataset', 'dataset_id',
+                            'description', 'facets_url',
+                            'full_name', 'id',
+                            'name', 'updated_at',
+                            'url'])
+        self.assertSetEqual(set(dataset_field.keys()), check_fields)
+
         facets = dataset_field.facets()
         # print "faces.total %s" % [facets.total] #  compare with ruby
         # We can get small or large numbers like 0 or 4902851621.0
-        self.assertTrue(isinstance(facets.total, Number) and
-                        facets.total >= 0,
-                        'facet should have an integer total field')
+        self.assertTrue(facets.total >= 0,
+                        'facet should have an total field >= 0')
 
 if __name__ == "__main__":
     unittest.main()
