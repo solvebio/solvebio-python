224ad5d0efefef1954bd5b0ac832710e5f3ec922
rocky <rocky@solvebio.com>
2014-10-23 12:12:42 -0400

    Check MD5 onn download

--- solvebio/test/test_annotation_access.py
+++ solvebio/test/test_annotation_access.py
@@ -5,6 +5,19 @@ import os
 from solvebio.resource import Annotation
 from solvebio.errors import SolveError
 
+import hashlib
+
+def md5_file(fileobj, blocksize=65535):
+    hasher = hashlib.md5()
+    fileobj.seek(0)
+    buf = fileobj.read(blocksize)
+
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = fileobj.read(blocksize)
+
+    return hasher.hexdigest()
+
 
 class AnnotationAccessTest(unittest.TestCase):
 
@@ -18,11 +31,14 @@ class AnnotationAccessTest(unittest.TestCase):
             return unittest.skip("no annotations found to download")
         ann = all.data[0]
         try:
-            response = ann.download(tempfile.tempdir)
+            response = ann.download(tempfile.gettempdir())
             self.assertEqual(response.status_code, 200,
                              "Download annotation file status ok")
             self.assertTrue(os.path.exists(response.filename),
                             "Download annotation file on filesystem")
+            vcf_md5 = md5_file(open(response.filename, 'rb'))
+            self.assertEqual(vcf_md5, 'e5ae61b8c6f334195d954423d1072ac1',
+                             "vcf_mdf on download")
             os.remove(response.filename)
         except SolveError as err:
             self.assertEqual(err.status_code, 404)
