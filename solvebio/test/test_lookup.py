23b4a1e9a7f6d46e73ac08609d7c743340e87561
Eyal Foni <eyalfoni@gmail.com>
2016-01-14 12:33:46 -0500

    changes to lookup test

--- solvebio/test/test_lookup.py
+++ solvebio/test/test_lookup.py
@@ -6,12 +6,8 @@ from .helper import SolveBioTestCase
 
 class LookupTests(SolveBioTestCase):
     # Using new version of HGNC for testing lookup
-    # becuase original test dataset did not contain sbids.
+    # because original test dataset did not contain sbids.
     TEST_DATASET_NAME = 'HGNC/2.1.2-2016-01-11/HGNC'
-
-    def setUp(self):
-        super(LookupTests, self).setUp()
-        self.dataset = Dataset.retrieve(self.TEST_DATASET_NAME)
 
     final_lookup_one = [{u'ccds_id': None,
                            u'cytogenetic_location': u'19q13.43',
@@ -89,24 +85,29 @@ class LookupTests(SolveBioTestCase):
                            u'uniprot_id': None,
                            u'vega_id': u'OTTHUMG00000168289'}]  # noqa
 
+    def setUp(self):
+        super(LookupTests, self).setUp()
+        self.dataset = Dataset.retrieve(self.TEST_DATASET_NAME)
+
     def test_lookup_error(self):
-        lookup_one = self.dataset.lookup('test')
-        lookup_two = self.dataset.lookup('test', 'nothing')
         # Check that incorrect lookup results in empty list.
+        lookup_one = self.dataset.lookup('test')
         self.assertEqual(lookup_one, [])
+
+        lookup_two = self.dataset.lookup('test', 'nothing')
         self.assertEqual(lookup_two, [])
 
     def test_lookup_correct(self):
-        sbid_one = '37133'
-        sbid_two = '27057'
-        lookup_one = self.dataset.lookup(sbid_one)
-        lookup_two = self.dataset.lookup(sbid_two)
         # Check that lookup with specific sbid is correct.
+        sbid_one = '37133'
+        lookup_one = self.dataset.lookup(sbid_one)
         self.assertEqual(lookup_one, self.final_lookup_one)
+
+        sbid_two = '27057'
+        lookup_two = self.dataset.lookup(sbid_two)
         self.assertEqual(lookup_two, self.final_lookup_two)
 
-        joint_lookup = self.dataset.lookup(sbid_one, sbid_two)
-
         # Check that combining sbids returns list of correct results.
+        joint_lookup = self.dataset.lookup(sbid_one, sbid_two)
         self.assertEqual(joint_lookup[0], self.final_lookup_one[0])
         self.assertEqual(joint_lookup[1], self.final_lookup_two[0])
