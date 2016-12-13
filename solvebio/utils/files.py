f15a2c31fd067a8dcc38ec3f2bd2858510fa9045
Jeff Hull <jsh2134@gmail.com>
2016-12-13 09:50:43 -0500

    remove deprecated U file open mode

--- solvebio/utils/files.py
+++ solvebio/utils/files.py
@@ -1,7 +1,7 @@
 def check_gzip(file_path):
     """Check if we have a gzipped file"""
     try:
-        with open(file_path, "U") as temp:
+        with open(file_path, 'rb') as temp:
             magic_check = temp.read(2)
             if magic_check != '\037\213':
                 return False
