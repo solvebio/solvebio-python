5d0537c9abbdd85c4d41d99d055066169cb8c8e2
Jeff Hull <jsh2134@gmail.com>
2017-03-15 14:31:19 -0400

    validate field data is pulled from templates

--- solvebio/test/test_shortcuts.py
+++ solvebio/test/test_shortcuts.py
@@ -17,6 +17,35 @@ class CLITests(SolveBioTestCase):
         email, token = main.main(['whoami'])
         self.assertEqual(token, os.environ.get('SOLVEBIO_API_KEY'))
 
+    def test_create_dataset(self):
+        # TODO mock client responses or allow for hard
+        # cleanup of dataset/depo
+
+        user = User.retrieve()
+        domain = user['account']['domain']
+        dataset_full_name = \
+            '{0}:test-client-{1}/1.0.0/test-{1}'.format(
+                domain, int(time.time()))
+        args = ['create-dataset', dataset_full_name,
+                   '--capacity', 'small']  # noqa
+        ds = main.main(args)
+        self.assertEqual(ds.full_name, dataset_full_name)
+
+        # does soft delete of depo
+        depo = Depository.retrieve(ds.depository_id)
+        depo.delete(soft=False)
+
+    def _validate_tmpl_fields(self, fields):
+        for f in fields:
+            if f.name == 'name':
+                self.assertEqual(f.entity_type, 'gene')
+            elif f.name == 'variants':
+                self.assertEqual(f.entity_type, 'variant')
+                self.assertEqual(f.is_list, True)
+                self.assertEqual(f.data_type, 'auto')
+            elif f.name == 'aliases':
+                self.assertEqual(f.data_type, 'string')
+
     def test_create_dataset_upload_template(self):
         # TODO mock client responses or allow for hard
         # cleanup of template and dataset/depo
@@ -33,6 +62,9 @@ class CLITests(SolveBioTestCase):
                    '--capacity', 'medium']  # noqa
         ds = main.main(args)
         self.assertEqual(ds.full_name, dataset_full_name)
+
+        # validate fields
+        self._validate_tmpl_fields(ds.fields())
 
         # does hard delete of template
         id_prefix = 'Created with dataset template: '
@@ -69,6 +101,9 @@ class CLITests(SolveBioTestCase):
                    '--capacity', 'large']  # noqa
         ds = main.main(args)
         self.assertEqual(ds.full_name, dataset_full_name)
+
+        # validate fields
+        self._validate_tmpl_fields(ds.fields())
 
         # cleanup
         tpl.delete()
