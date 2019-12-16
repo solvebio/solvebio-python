e1d6f1a21627f6b51d9dd777beb4cac2ddd74d0b
David Caplan <dcaplan@solvebio.com>
2019-12-16 10:36:32 -0500

    print the proper status code and error message (closes #320)

--- solvebio/errors.py
+++ solvebio/errors.py
@@ -23,36 +23,41 @@ class SolveError(Exception):
         self.status_code = None
         self.message = message or self.default_message
         self.field_errors = []
-        self.response = response  # For clients that want the full story.
+        self.response = response
 
         if response is not None:
             self.status_code = response.status_code
             try:
                 self.json_body = response.json()
-            except:
-                if response.status_code == 404:
-                    self.message = '404 Not Found.'
-                logger.debug(
-                    'API Response (%d): No content.' % self.status_code)
-            else:
                 logger.debug(
                     'API Response (%d): %s'
                     % (self.status_code, self.json_body))
+            except:
+                self.json_body = ''
+                logger.debug(
+                    'API Response (%d): Response does not contain JSON.'
+                    % self.status_code)
 
-                if self.status_code in [400, 401, 403, 404]:
-                    self.message = 'Bad request.'
+            if self.status_code == 400:
+                self.message = 'Bad Request'
+            elif response.status_code == 401:
+                self.message = '401 Unauthorized'
+            elif response.status_code == 403:
+                self.message = '403 Forbidden'
+            elif response.status_code == 404:
+                self.message = '404 Not Found'
 
-                    if 'detail' in self.json_body:
-                        self.message = '%s' % self.json_body['detail']
-                        del self.json_body['detail']
+            if 'detail' in self.json_body:
+                self.message = '%s' % self.json_body['detail']
+                del self.json_body['detail']
 
-                    if 'non_field_errors' in self.json_body:
-                        self.message = '%s' % \
-                            ', '.join(self.json_body['non_field_errors'])
-                        del self.json_body['non_field_errors']
+            if 'non_field_errors' in self.json_body:
+                self.message = '%s' % \
+                    ', '.join(self.json_body['non_field_errors'])
+                del self.json_body['non_field_errors']
 
-                    if self.json_body:
-                        self.message += ' %s' % self.json_body
+                if self.json_body:
+                    self.message += ' %s' % self.json_body
 
     def __str__(self):
         return self.message
