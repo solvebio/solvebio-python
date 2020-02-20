e09e28c1ea749cab36305cdbdb011b8a75cc23f7
Jeff Hull <jeff@solvebio.com>
2020-02-20 16:04:56 -0500

    update tests

--- solvebio/cli/auth.py
+++ solvebio/cli/auth.py
@@ -1,41 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import print_function
-from six.moves import input as raw_input
-
-import getpass
 
 import solvebio
-from ..client import client, SolveError
+from ..client import client
 from .credentials import get_credentials
 from .credentials import save_credentials
 from .credentials import delete_credentials
-
-
-def _ask_for_credentials():
-    """
-    Asks the user for their email and password.
-    """
-    print('Please enter your SolveBio credentials')
-    domain = raw_input('Domain (e.g. <domain>.solvebio.com): ')
-    # Check to see if this domain supports password authentication
-    try:
-        account = client.request('get', '/p/accounts/{}'.format(domain))
-        auth = account['authentication']
-    except:
-        raise SolveError('Invalid domain: {}'.format(domain))
-
-    # Account must support password-based login
-    if auth.get('login') or auth.get('SAML', {}).get('simple_login'):
-        email = raw_input('Email: ')
-        password = getpass.getpass('Password (typing will be hidden): ')
-        return (domain, email, password)
-    else:
-        print(
-            'Your domain uses Single Sign-On (SSO). '
-            'Please visit https://{}.solvebio.com/settings/security '
-            'for instructions on how to log in.'.format(domain))
-        return None
 
 
 def login_and_save_credentials(*args, **kwargs):
