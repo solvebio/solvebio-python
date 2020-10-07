327110308b0c6de0fd8b7a83095764ae8c0b5862
tatjana <tatjana@solvebio.com>
2020-10-07 14:45:52 +0200

    Unittests updates

--- recipes/tests/test_recipes_sync.py
+++ recipes/tests/test_recipes_sync.py
@@ -46,47 +46,47 @@ def test_sync_recipe(mock_dataset_template_retrieve,
     # Invalid file
     with pytest.raises(SystemExit) as e:
         config = get_input("non_existing_file.yml")
-        sync_recipes.sync_recipes(["--mode", "sync", "--name", "cDNA Change (v1.0.2)", config])
+        sync_recipes.sync(["--name", "cDNA Change (v1.0.2)", config])
     assert e.value.code == 2
 
     # Valid commands
     config = get_input("valid.yml")
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "sync", "--name", "cDNA Change (v1.0.2)", config],
+    result = runner.invoke(sync_recipes.sync,
+                           args=["--name", "cDNA Change (v1.0.2)", config],
                            input="y")
     assert result.exit_code == 0
     assert "create cDNA Change (v1.0.2)" in result.output
 
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "delete", "--name", "cDNA Change (v1.0.2)", config],
+    result = runner.invoke(sync_recipes.delete,
+                           args=["--name", "cDNA Change (v1.0.2)", config],
                            input="y")
     assert result.exit_code == 0
     assert "Requested recipe cDNA Change (v1.0.2) doesn't exist in SolveBio!" in result.output
 
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "sync", "--all", config],
+    result = runner.invoke(sync_recipes.sync,
+                           args=["--all", config],
                            input="N")
     assert result.exit_code == 0
     assert "create cDNA Change (v1.0.2)" in result.output
     assert "Aborted" in result.output
 
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "delete", "--all", config],
+    result = runner.invoke(sync_recipes.delete,
+                           args=["--all", config],
                            input="y")
     assert result.exit_code == 0
     assert "Requested recipe cDNA Change (v1.0.2) doesn't exist in SolveBio!" in result.output
 
     config2 = get_input("valid2.yml")
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "sync", "--all", config2],
+    result = runner.invoke(sync_recipes.sync,
+                           args=["--all", config2],
                            input="y")
 
     assert result.exit_code == 0
     assert "create Gene (v1.0.3)" in result.output
     assert "create Protein Change (v1.0.4)" in result.output
 
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "delete", "--all", config2],
+    result = runner.invoke(sync_recipes.delete,
+                           args=["--all", config2],
                            input="y")
     assert result.exit_code == 0
     assert "Requested recipe Gene (v1.0.3) doesn't exist in SolveBio!" in result.output
@@ -94,11 +94,7 @@ def test_sync_recipe(mock_dataset_template_retrieve,
 
     yml_export_file = get_input("export.yml")
     # Test export mode with public/account recipes
-    result = runner.invoke(sync_recipes.sync_recipes, args=["--mode", "export", "--all", yml_export_file])
-    assert result.exit_code == 2
-    assert "Export mode should be used" in result.output
-
-    result = runner.invoke(sync_recipes.sync_recipes,
-                           args=["--mode", "export", "--public-recipes", yml_export_file])
+    result = runner.invoke(sync_recipes.export,
+                           args=["--public-recipes", yml_export_file])
     os.remove(yml_export_file)
     assert "Exporting all public recipes" in result.output
