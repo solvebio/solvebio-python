1274c3d7c39c01f27052938f597a20e4c5badc70
Nikola Krivacevic <nkrivacevic@solvebio.com>
2021-10-29 16:04:43 +0200

    add new demo app that uses solve client

--- solvebio/contrib/streamlit/app.py
+++ solvebio/contrib/streamlit/app.py
@@ -1,19 +1,66 @@
 import streamlit as st
+import pandas as pd
+
 from solvebio_streamlit import SolveBioStreamlit
 
 
-def streamlit_demo_app():
-    st.title("Solvebio app")
-    option = st.radio("Select SolveBio platform:", ("Hive", "Mesh", "Core"))
+@st.cache
+def get_personal_vault_items(vault):
+    """Getting items from personal vaulet (caching using Streamlit cache mechanism)"""
 
-    if option == "Hive":
-        st.write(
-            "HIVE is an on-demand network of individually-vetted genomics specialists."
-        )
-    elif option == "Mesh":
-        st.write("MESH is a dynamic, web-based interface for genomics information.")
-    elif option == "Core":
-        st.write("CORE is a system of technologies for managing genomic data at scale.")
+    files = vault.files()
+    folders = vault.folders()
+    datasets = vault.datasets()
+
+    return files, folders, datasets
+
+
+def streamlit_demo_app():
+    """Streamlit demo app
+
+    It fetches the SolveClient (initialised with OAuth2 token) from Streamlit session state
+    and makes API call through that client.
+    """
+
+    # Getting the sovle client from the Streamlit session state
+    solve_client = st.session_state.solve_client
+
+    st.title("Solvebio app")
+
+    # SolveBio user
+    user = solve_client.User.retrieve()
+    st.header(f"{user['first_name']}'s personal vault overview:")
+
+    # Personal vault
+    vault = solve_client.Vault.get_personal_vault()
+    st.write(vault["description"])
+
+    files, folders, datasets = get_personal_vault_items(vault)
+
+    # Visualising the stats from personal vault
+    data = {
+        "Number of files": [files["total"]],
+        "Number of datasets": [datasets["total"]],
+        "Number of folders": [folders["total"]],
+    }
+    chart_data = pd.DataFrame.from_dict(
+        data, columns=["Number of items"], orient="index"
+    )
+    st.write(chart_data)
+    st.bar_chart(chart_data)
+
+    # Listing items from personal vault
+    st.subheader("List selected items")
+    option = st.radio("Select SolveBio platform:", ("Files", "Folders", "Datasets"))
+    if option == "Files":
+        for item in files:
+            st.markdown(f"- {item['filename']}")
+    elif option == "Folders":
+        for item in folders:
+            st.markdown(f"- {item['filename']}")
+    elif option == "Datasets":
+        for item in datasets:
+            st.markdown(f"- {item['filename']}")
 
 
 # Wrapping Streamlit app with SolveBio OAuth2
