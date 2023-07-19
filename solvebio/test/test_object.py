78ec4640a3bb00aa4446e71be93fd931bda52095
Nikola Rasulic <nikola.rasulic@precisionformedicine.com>
2023-07-19 20:32:17 +0200

    update tests

--- solvebio/test/test_object.py
+++ solvebio/test/test_object.py
@@ -321,6 +321,47 @@ class ObjectTests(SolveBioTestCase):
 
         self.assertEqual(list(resp), valid_response['results'])
 
+    def test_object_versioning(self):
+        vault = self.client.Vault.get_personal_vault()
+        initial_versioning_status = vault["versioning"]
+        vault.enable_versioning()
+
+        file = self.client.Object.create(filename='solvebio-py-test.txt',
+                                         object_type='file',
+                                         vault_id=vault.id)
+
+        self.addCleanup(ObjectTests.clean_up_after_object_versioning, vault, file, initial_versioning_status)
+
+        assert file['version_count'] == 1
+
+        file = self.client.Object.create(filename='solvebio-py-test.txt',
+                                         object_type='file',
+                                         vault_id=vault.id)
+
+        assert file['version_count'] == 2
+
+        versions = file.list_versions()
+        assert len(versions['data']) == 2
+
+        version_id_1 = versions['data'][0]['id']
+
+        file.delete_version(version_id_1)
+        versions = file.list_versions(include_deleted=False)
+        assert len(versions['data']) == 1
+
+        versions = file.list_versions()
+        assert versions['data'][0]['is_deleted']
+
+        file.undelete_version(version_id_1)
+        versions = file.list_versions()
+        assert not versions['data'][0]['is_deleted']
+
+    @staticmethod
+    def clean_up_after_object_versioning(vault, file, versioning_status):
+        from .test_vault import VaultTests
+        file.delete(force=True)
+        VaultTests.clean_up_after_vault_versioning(vault, versioning_status)
+
 
 class ObjectUploadTests(SolveBioTestCase):
 
@@ -418,45 +459,4 @@ class ObjectUploadTests(SolveBioTestCase):
         download_url = new_obj.download_url()
         response = requests.request(method='get', url=download_url)
         self.assertEqual(response.content.decode('utf-8'), "sample file updated")
-
-    def test_object_versioning(self):
-        vault = self.client.Vault.get_personal_vault()
-        initial_versioning_status = vault["versioning"]
-        vault.enable_versioning()
-
-        file = self.client.Object.create(filename='solvebio-py-test.txt',
-                                         object_type='file',
-                                         vault_id=vault.id)
-
-        self.addCleanup(ObjectTests.clean_up_after_object_versioning, vault, file, initial_versioning_status)
-
-        assert file['version_count'] == 1
-
-        file = self.client.Object.create(filename='solvebio-py-test.txt',
-                                         object_type='file',
-                                         vault_id=vault.id)
-
-        assert file['version_count'] == 2
-
-        versions = file.list_versions()
-        assert len(versions['data']) == 2
-
-        version_id_1 = versions['data'][0]['id']
-
-        file.delete_version(version_id_1)
-        versions = file.list_versions(include_deleted=False)
-        assert len(versions['data']) == 1
-
-        versions = file.list_versions()
-        assert versions['data'][0]['is_deleted']
-
-        file.undelete_version(version_id_1)
-        versions = file.list_versions()
-        assert not versions['data'][0]['is_deleted']
-
-    @staticmethod
-    def clean_up_after_object_versioning(vault, file, versioning_status):
-        from .test_vault import VaultTests
-        file.delete(force=True)
-        VaultTests.clean_up_after_vault_versioning(vault, versioning_status)
         
