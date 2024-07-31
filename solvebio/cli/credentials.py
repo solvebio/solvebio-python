84ef3c1012968570af49d4b60067fabd4877f6a7
David Caplan <dcaplan@solvebio.com>
2024-07-31 10:50:03 -0400

    fix python3 issue related to empty credentials file

--- solvebio/cli/credentials.py
+++ solvebio/cli/credentials.py
@@ -17,23 +17,22 @@ class netrc(_netrc):
     """
     Adds a save() method to netrc
     """
+
     @staticmethod
     def path():
-        if os.name == 'nt':
+        if os.name == "nt":
             # Windows
-            path = '~\\_solvebio\\credentials'
+            path = "~\\_solvebio\\credentials"
         else:
             # *nix
-            path = '~/.solvebio/credentials'
+            path = "~/.solvebio/credentials"
 
         try:
             path = os.path.expanduser(path)
         except KeyError:
             # os.path.expanduser can fail when $HOME is undefined and
             # getpwuid fails. See http://bugs.python.org/issue20164
-            raise IOError(
-                "Could not find any home directory for '{0}'"
-                .format(path))
+            raise IOError("Could not find any home directory for '{0}'".format(path))
 
         if not os.path.isdir(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
@@ -41,10 +40,12 @@ class netrc(_netrc):
         # create an empty credentials file if it doesn't exist
         if not os.path.exists(path):
             try:
-                open(path, 'a').close()
+                open(path, "a").close()
             except IOError:
-                raise Exception("Could not create a SolveBio credentials file"
-                                " at '%s', permission denied." % path)
+                raise Exception(
+                    "Could not create a SolveBio credentials file"
+                    " at '%s', permission denied." % path
+                )
         return path
 
     def save(self, path):
@@ -52,13 +53,14 @@ class netrc(_netrc):
         rep = ""
         for host in self.hosts.keys():
             attrs = self.hosts[host]
-            rep = rep + "machine " + host + "\n\tlogin " \
-                + six.text_type(attrs[0]) + "\n"
+            rep = (
+                rep + "machine " + host + "\n\tlogin " + six.text_type(attrs[0]) + "\n"
+            )
             if attrs[1]:
                 rep = rep + "\taccount " + six.text_type(attrs[1]) + "\n"
             rep = rep + "\tpassword " + six.text_type(attrs[2]) + "\n"
 
-        f = open(path, 'w')
+        f = open(path, "w")
         f.write(rep)
         f.close()
 
@@ -67,6 +69,7 @@ class CredentialsError(BaseException):
     """
     Raised if the credentials are not found.
     """
+
     pass
 
 
@@ -81,8 +84,7 @@ def get_credentials():
         if not netrc_obj.hosts:
             return None
     except (IOError, TypeError, NetrcParseError) as e:
-        raise CredentialsError(
-            'Could not open credentials file: ' + str(e))
+        raise CredentialsError("Could not open credentials file: " + str(e))
 
     host = as_netrc_machine(solvebio.api_host)
     if host in netrc_obj.hosts:
@@ -91,7 +93,7 @@ def get_credentials():
 
     # If the preferred host is not the global default, don't try
     # to select any other.
-    if host != 'api.solvebio.com':
+    if host != "api.solvebio.com":
         return None
 
     # If there are no stored credentials for the default host,
@@ -99,12 +101,16 @@ def get_credentials():
     # available option that ends with '.api.solvebio.com',
     # Otherwise use the first available.
     for h in netrc_obj.hosts:
-        if h.endswith('.api.solvebio.com'):
-            return ('https://' + h,) + netrc_obj.authenticators(h)
+        if h.endswith(".api.solvebio.com"):
+            return ("https://" + h,) + netrc_obj.authenticators(h)
 
     # Return the first available
-    host = netrc_obj.hosts.keys()[0]
-    return ('https://' + host,) + netrc_obj.authenticators(host)
+    hosts = list(netrc_obj.hosts.keys())
+    if hosts:
+        host = hosts[0]
+        return ("https://" + host,) + netrc_obj.authenticators(host)
+
+    return None
 
 
 def delete_credentials():
@@ -112,7 +118,7 @@ def delete_credentials():
         netrc_path = netrc.path()
         rc = netrc(netrc_path)
     except (IOError, TypeError, NetrcParseError) as e:
-        raise CredentialsError('Could not open netrc file: ' + str(e))
+        raise CredentialsError("Could not open netrc file: " + str(e))
 
     try:
         del rc.hosts[as_netrc_machine(solvebio.api_host)]
@@ -122,14 +128,14 @@ def delete_credentials():
         rc.save(netrc_path)
 
 
-def save_credentials(email, token, token_type='Token', api_host=None):
+def save_credentials(email, token, token_type="Token", api_host=None):
     api_host = api_host or solvebio.api_host
 
     try:
         netrc_path = netrc.path()
         rc = netrc(netrc_path)
     except (IOError, TypeError, NetrcParseError) as e:
-        raise CredentialsError('Could not open netrc file: ' + str(e))
+        raise CredentialsError("Could not open netrc file: " + str(e))
 
     # Overwrites any existing credentials
     rc.hosts[as_netrc_machine(api_host)] = (email, token_type, token)
